a76c21b60a65bf40eaf3de4f1545165ef357486b
Rubén Aguilera <ruben.aguilera@kairosds.com>
2020-03-21 13:10:33 +0100

    end guard canactivate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AuthService } from './auth.service';
 
 const ROUTES: Routes =  [
   {path: '', redirectTo: 'directives', pathMatch: 'full'},
-  {path: 'directives',
+  {path: 'directives', canActivate: [AuthService],
     loadChildren: () => import('./directives/directives.module').then(m => m.DirectivesModule)},
-  {path: 'databinding',
+  {path: 'databinding', canActivate: [AuthService],
      loadChildren: () => import('./databinding/databinding.module').then(m => m.DatabindingModule)},
   {path: '**', redirectTo: 'directives'}
 ];
